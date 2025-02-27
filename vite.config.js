2b9dc046b98c4face1481f058dbdef8b5741bc41
adeeb jamil <2100032535@kluniversity.in>
2025-02-27 17:36:20 +0530

    up

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port:3000,
     proxy:{
       '/api':{
-        target:'http://localhost:5000',
+        target:'https://chat-server-1-bfoj.onrender.comm',
         changeOrigin: true,
         secure: false,
       }
