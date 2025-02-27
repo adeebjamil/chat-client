1eebd37e0a623c55522259fb1338e88cadb000e3
adeeb jamil <2100032535@kluniversity.in>
2025-02-27 17:59:04 +0530

    up

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
         changeOrigin: true,
         secure: false,
       }
-    }
+    },
+    allowedHosts: ['chat-client-irnt.onrender.com'] // Add your Render client URL here
   },
 })
